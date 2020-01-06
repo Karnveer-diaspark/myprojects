7f256e2555d21f68ef3787389ebe32ab76cd1496
Karnveer Singh. Mourya <Karnveer.Mourya@diasparkindia.com>
2020-01-06 15:31:23 +0530

    php

---
+++ all/MyProject/LoginAuth.php
@@ -0,0 +1,17 @@
+<?php
+include 'config/database.php';
+
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+$count = mysqli_num_rows($result);
+
+if ($count == 1){
+
+//echo "Login Credentials verified";
+echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
+
+}else{
+echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
+//echo "Invalid Login Credentials";
+}
+
+?>
